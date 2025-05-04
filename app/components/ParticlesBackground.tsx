3e1fafe8df5b262992dbef35b03910a8c2ffacbe
Samarth Bargale <samarthbargale@gmail.com>
2025-05-04 14:24:59 -0700

    Update tsParticles configuration

--- app/components/ParticlesBackground.tsx
+++ app/components/ParticlesBackground.tsx
@@ -15,13 +15,10 @@ export default function ParticlesBackground() {
       id="tsparticles"
       init={particlesInit}
       options={{
-        background: {
-          color: {
-            value: 'transparent',
-          },
-        },
-        fpsLimit: 120,
         particles: {
+          number: {
+            value: 80,
+          },
           color: {
             value: '#ffffff',
           },
@@ -34,31 +31,15 @@ export default function ParticlesBackground() {
           },
           move: {
             enable: true,
-            outModes: {
-              default: 'bounce',
-            },
-            random: false,
             speed: 1,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
           },
           opacity: {
             value: 0.2,
-          },
-          shape: {
-            type: 'circle',
           },
           size: {
             value: { min: 1, max: 3 },
           },
         },
-        detectRetina: true,
       }}
       className="absolute inset-0 -z-10"
     />
